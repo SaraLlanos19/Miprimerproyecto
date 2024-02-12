c65954245bcbdf4da24df0a1f5228194b3e6e4e5
SaraLlanos19 <u20232218161@usco.edu.co>
2024-02-11 21:27:22 -0500

    Creacion del proyecto

---
+++ src/miprimerproyecto/EjemploPedirDatos.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package miprimerproyecto;
+import java.util.Scanner;  // Importa la clase Scanner
+/**
+ *
+ * @author Sara Llanos Morea
+ */
+public class EjemploPedirDatos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.println("Ingresar altura de la persona en cm");
+        String alturaDeLaPersona = scanner.nextLine();
+        System.out.println("la altura de la persona es: " + alturaDeLaPersona);
+        
+        System.out.println("Ingresar el peso de la persona en kg");
+        String pesoDeLaPersona = scanner.nextLine();
+        System.out.println("el peso de la persona es: " + pesoDeLaPersona);
+        
+        System.out.println("Peso: " + pesoDeLaPersona  +  ". Altura: " + alturaDeLaPersona + ".");
+        
+        
+
+                
+    }
+    
+}
